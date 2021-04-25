ba456839c6cda9e35968edfca3c1976be293864b
aramaditya <adityaram532@gmail.com>
2021-04-25 13:30:21 +0530

    added travis yml file

--- Dockerfile
+++ Dockerfile
@@ -9,3 +9,4 @@ RUN npm run build
 FROM nginx
 COPY --from=0 /app/build /usr/share/nginx/html
 
+
--- src/App.test.js
+++ src/App.test.js
@@ -12,3 +12,16 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
